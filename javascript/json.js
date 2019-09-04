08ddec425abc068d6eabb899a622eff3bdd66931
SEJ <sej@needrent.dk>
2019-09-04 14:55:51 +0200

    added some HTML and CSS

--- javascript/json.js
+++ javascript/json.js
@@ -1,9 +1,7 @@
-// products JSON
-//const link = "https://spreadsheets.google.com/feeds/list/10k5FHg8AolohKoybFTJarvWLSzhzOe-jr8HbHa6KZwU/od6/public/values?alt=json";
+// Fetch student data
+// class list add house
+let myLink = "https://petlatkea.dk/2019/students1991.json";
 
-let myLink = "http://petlatkea.dk/2019/students1991.json";
-
-const template = document.querySelector("template").content;
 const parent = document.querySelector("main");
 const list = document.querySelector("main ul");
 
@@ -15,19 +13,40 @@ function loadData(link) {
 
 function show(data) {
   data.forEach(object => {
-    console.log(object);
+    //console.log(data);
     // clone the template
-    const clone = template.cloneNode(true);
 
     // create list
     let newLi = document.createElement("li");
 
-    // populate the template
-    clone.querySelector("ul li").textContent = object.fullname;
-    clone.querySelector("h3").textContent = object.house;
+    let name = object.fullname;
+    let house = object.house;
+
+    newLi.textContent = name + ", " + house;
 
     // append to the DOM
-    parent.appendChild(clone);
+    list.appendChild(newLi);
   });
+  const newArr = new Array(document.querySelectorAll("main ul li"));
+
+  console.log(newArr["0"]["0"].innerText);
 }
 loadData(myLink);
+
+// sort first name by asc (.sort)
+// if first name is asc use desc (.reverse)
+
+// sort last name by asc (.sort)
+// if last name is asc use desc (.reverse)
+
+// set filter all to standard
+// show all
+// if gryffindor is clicked, hide hufflepuff, ravenclaw, slytherin
+// if else hufflepuff is clicked, hide gryffindor, ravenclaw, slytherin
+// if else ravenclaw is clicked, hide hufflepuff, gryffindor, slytherin
+// if else slytherin is clicked, hide hufflepuff, ravenclaw, gryffindor
+
+//modal make student click event (names and house)
+//Open model
+
+//click modal to close
